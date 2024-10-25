e47b1b7ff85f6a7a252972392400851437a766df
CryptoCurious31 <info.cryptocurious@gmail.com>
2024-10-25 05:49:51 +0500

    My first Javascript Own Login for check Palindrome

--- Palindrome-Checker/script.js
+++ Palindrome-Checker/script.js
@@ -1,2 +1,17 @@
 const input = document.getElementById('palindrome-checker');
 const submitButton = document.getElementById('check-palindrome');
+
+
+const checkInput = () => {
+
+    const answer = document.createElement('p');
+    answer.classList.add('hint-text');
+
+    if(input[0] === input.length - 1){
+        answer.textContent = `${input.value} is a palindrome.`;
+    } else if (input[0] !== input.length - 1) {
+        answer.textContent = `${input.value} is not a palindrom`;
+    }
+}
+
+submitButton.addEventListener('click', checkInput);
